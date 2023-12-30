2909d7c59bd32ab5f90f8733b47dd4740553689e
Onur Haktan <onurhaktan243@gmail.com>
2023-12-30 15:15:39 +0300

    change badges and add new project

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = "z67vfiw2";
 const dataset = "production";
-const apiVersion = "2022-03-07";
+const apiVersion = "1";
 
 export const client = createClient({
   projectId,
