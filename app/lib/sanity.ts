ebb920206dc32f722398817d0e34698c8e3c2f78
Onur Haktan <onurhaktann@gmail.com>
2024-10-05 00:45:48 +0300

    add new project

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = "z67vfiw2";
 const dataset = "production";
-const apiVersion = "2024-03-13";
+const apiVersion = "2022-03-07";
 
 export const client = createClient({
   projectId,
