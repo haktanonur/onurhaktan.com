eacff2a43faa9c6dc37cd711b048aff7bafd170f
Onur Haktan <onurhaktann@gmail.com>
2024-10-05 01:25:21 +0300

    change db version

--- app/lib/sanity.ts
+++ app/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 const projectId = "z67vfiw2";
 const dataset = "production";
-const apiVersion = "2022-03-07";
+const apiVersion = "2021-10-21";
 
 export const client = createClient({
   projectId,
